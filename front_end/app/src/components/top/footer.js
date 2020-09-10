3f14bf9f49c53ad306464d0832342fe517e0fd3c
rurito0125 <rurito.blog@gmail.com>
2020-09-10 11:58:19 +0900

    画像追加

--- front_end/app/src/components/top/footer.js
+++ front_end/app/src/components/top/footer.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
 const Footer = () => {
   return (
     <div>
+      <div>
+        <img src={`${window.location.origin}/images/sample.png`} />
+      </div>
       <p>this is footer</p>
     </div>
   )
